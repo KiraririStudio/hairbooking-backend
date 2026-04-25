d3c477fe2dd551b6e1acc20f0187e28046223354
KiraririStudio <82488681+KiraririStudio@users.noreply.github.com>
2026-04-25 10:19:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -256,14 +256,14 @@ def admin():
         html += f"<td>{len(people)} / 1</td><td>"
 
         
-    for idx, p in enumerate(people, 1):
-        html += f"""
-        {idx}. {p['name']}｜{p['phone']}｜{p['code']}
-        <form method="post" action="/admin/delete/{p['id']}"
-              onsubmit="return confirm('確定要刪除這筆預約嗎？');">
-            <button type="submit">刪除</button>
-        </form><br>
-        """
+        for idx, p in enumerate(people, 1):
+            html += f"""
+            {idx}. {p['name']}｜{p['phone']}｜{p['code']}
+            <form method="post" action="/admin/delete/{p['id']}"
+                  onsubmit="return confirm('確定要刪除這筆預約嗎？');">
+                <button type="submit">刪除</button>
+            </form><br>
+            """
 
 
         html += "</td></tr>"
