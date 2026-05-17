c2fef1278fa490d13d9f5a98fb7ba6f9295ddae5
KiraririStudio <82488681+KiraririStudio@users.noreply.github.com>
2026-05-17 16:13:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
